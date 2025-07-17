3cc2224eec26c657134a76619ef68bb455e86999
HuynhNgocKhanh <huynhngockhanh1201@gmail.com>
2025-07-17 13:38:18 +0700

    Update small

--- finalProject/Assets/Scrips/Arow.cs
+++ finalProject/Assets/Scrips/Arow.cs
@@ -3,47 +3,58 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Arow : MonoBehaviour
 {
-    GameObject player;
-    Rigidbody2D rb;
-    SpriteRenderer sprite;
-    public float force;
-    private audio music;
-  private void Awake()
-  {
-    music = GameObject.FindGameObjectWithTag("audio").GetComponent<audio>();
-  }
+	GameObject player;
+	Rigidbody2D rb;
+	SpriteRenderer sprite;
+	public float force;
+	private audio music;
 
+	private float direc;
 
-    private float direc;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.Find("Player");
-        sprite = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        sprite.flipX = player.GetComponent<SpriteRenderer>().flipX;
-        rb.linearVelocity = new Vector2((direc = sprite.flipX ? -1 : 1) * force, rb.linearVelocity.y);
-        //Hủy object sau 3s - chỉnh từ 1f lên 2f
-        Invoke("DestroyGameObject", 2f);
-    }
-    void DestroyGameObject()
-    {
-        Destroy(gameObject);
-    }
+	private void Awake()
+	{
+		music = GameObject.FindGameObjectWithTag("audio").GetComponent<audio>();
+	}
 
-    void OnTriggerEnter2D(Collider2D a)
-    {
-        if (a.gameObject.tag == "thuhoang")
-        {
-            GameObject.Find("text_thuhoang").GetComponent<Score>().UpdateScore();
-// Điểm tăng khi va chạm
-           
-            Destroy(a.gameObject);
-            DestroyGameObject();
-            music.PlaySfnhac(music.nhacbanchetthuhoang);
-        } 
+	// Start is called before the first frame update
+	void Start()
+	{
+		player = GameObject.Find("Player");
+		sprite = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D>();
+		sprite.flipX = player.GetComponent<SpriteRenderer>().flipX;
 
-    }
+		// Tăng tốc độ bay
+		rb.linearVelocity = new Vector2((direc = sprite.flipX ? -1 : 1) * force * 2f, 0);
+
+		// Không va chạm với người chơi
+		Collider2D playerCol = player.GetComponent<Collider2D>();
+		Collider2D bulletCol = GetComponent<Collider2D>();
+		if (playerCol != null && bulletCol != null)
+		{
+			Physics2D.IgnoreCollision(playerCol, bulletCol);
+		}
+
+		// Giảm thời gian tồn tại mũi tên
+		Invoke("DestroyGameObject", 1.2f);
+	}
+
+	void DestroyGameObject()
+	{
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D a)
+	{
+		if (a.gameObject.tag == "thuhoang")
+		{
+			GameObject.Find("text_thuhoang").GetComponent<Score>().UpdateScore();
+			Destroy(a.gameObject);
+			DestroyGameObject();
+			music.PlaySfnhac(music.nhacbanchetthuhoang);
+		}
+	}
 }
--- finalProject/Assets/Scrips/prefabArow.cs
+++ finalProject/Assets/Scrips/prefabArow.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 
 public class PrefabBullet : MonoBehaviour
 {
-    public GameObject bullet;
-    public GameObject bulletTransform;
-    public float cooldownFire = 0;
-    bool canFire;
-    float Timer = 0;
-    SpriteRenderer sprite;
-    void Start (){
-        sprite= bulletTransform.GetComponent<SpriteRenderer>();
-    }
+	public GameObject bullet;
+	public GameObject bulletTransform;
+	public float cooldownFire = 0;
+	bool canFire;
+	float Timer = 0;
+	SpriteRenderer sprite;
+	void Start()
+	{
+		sprite = bulletTransform.GetComponent<SpriteRenderer>();
+	}
 
-    void Update()
-    {
-        Timer += Time.deltaTime;
-        canFire = Timer > cooldownFire ? true : false;
-        if (canFire && Input.GetKeyDown(KeyCode.H))
-        {
-            Timer = 0;
-            InstantiateBullet();
-        }
+	void Update()
+	{
+		Timer += Time.deltaTime;
+		canFire = Timer > cooldownFire ? true : false;
+		if (canFire && Input.GetKeyDown(KeyCode.H))
+		{
+			Timer = 0;
+			InstantiateBullet();
+		}
 
-    }
+	}
 
-    void InstantiateBullet()
-    {   Vector3 prefabPos = bulletTransform.transform.position + Change() - new Vector3(0,0.9f,0);
-        Instantiate(bullet, prefabPos, Quaternion.identity);
-    }
+	void InstantiateBullet()
+	{
+		Vector3 prefabPos = bulletTransform.transform.position + Change() - new Vector3(0, 0.9f, 0);
+		Instantiate(bullet, prefabPos, Quaternion.identity);
+	}
 
-    Vector3 Change()
-    {
-        if(!sprite.flipX)
-         return new Vector3(1f,0f,0f);
-        else
-         return new Vector3(-1f,0f,0f);
-    }
+	Vector3 Change()
+	{
+		if (!sprite.flipX)
+			return new Vector3(1f, 0f, 0f);
+		else
+			return new Vector3(-1f, 0f, 0f);
+	}
 
 }
